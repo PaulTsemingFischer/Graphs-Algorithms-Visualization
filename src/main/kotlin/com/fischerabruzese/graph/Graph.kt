78dbd4ff51cacadf90648973e8b8b5b370c45b6b
Luke Abruzese <24labruzese@xbhs.net>
2023-12-12 09:20:27 -0500

    Vertices and edgeMatrix initialization. Still has a few problems with from/to specifications

--- src/main/kotlin/com/fischerabruzese/graph/Graph.kt
+++ src/main/kotlin/com/fischerabruzese/graph/Graph.kt
@@ -1,18 +1,34 @@
 package com.fischerabruzese.graph
-data class Edge<E:Any>(val from: E, val to: E, val weight: Int)
+
+import javafx.scene.transform.MatrixType
+
+data class Edge<E:Any>(val from: E, val to: E?, val weight: Int)
 
 
 class Graph<E:Any>(edges: Collection<Edge<E>>) {
-    //constructor(vararg items: E) : this(items.toList())
+    constructor(vararg edge: Edge<E>) : this(edge.toList())
     constructor() : this(emptyList())
 
     data class Vertex<E>(val item: E, val index: Int)
 
-    private var vertices : ArrayList<Vertex<E>>
+    private var vertices : ArrayList<Vertex<E>> = ArrayList()
+    private var edgeMatrix : Array<IntArray>
+
     init {
-        vertices = ArrayList<Vertex<E>>().apply {
-            for ((i, edge) in edges.withIndex())
-                add(Vertex(edge, i))
+        val addedVertices = HashMap<E, Int>()
+        for(edge in edges){
+            if(addedVertices.put(edge.from, addedVertices.size+1) == null){
+                vertices.add(Vertex(edge.from, addedVertices.size))
+            }
         }
+        edgeMatrix = Array(vertices.size) {IntArray(vertices.size)}
+        for(edge in edges){
+            if(edge.to != null){
+                edgeMatrix[addedVertices[edge.to]!!][addedVertices[edge.from]!!] = edge.weight
+                //if symmetric
+                edgeMatrix[addedVertices[edge.from]!!][addedVertices[edge.to]!!] = edge.weight
+            }
+        }
+
     }
 }
