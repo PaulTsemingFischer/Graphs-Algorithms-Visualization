19c29d836d20a987174e925446c422aebe6b5424
Paul F <paultsemingfischer@gmail.com>
2023-12-11 23:43:52 -0500

    Starting Graph

--- src/main/kotlin/com/fischerabruzese/graph/Graph.kt
+++ src/main/kotlin/com/fischerabruzese/graph/Graph.kt
@@ -1,10 +1,18 @@
 package com.fischerabruzese.graph
+data class Edge<E:Any>(val from: E, val to: E, val weight: Int)
 
-class Graph<E:Any>(vararg item: E) {
-    constructor(items: Array<E>) : this(*items)
 
+class Graph<E:Any>(edges: Collection<Edge<E>>) {
+    //constructor(vararg items: E) : this(items.toList())
+    constructor() : this(emptyList())
+
+    data class Vertex<E>(val item: E, val index: Int)
+
+    private var vertices : ArrayList<Vertex<E>>
     init {
-
+        vertices = ArrayList<Vertex<E>>().apply {
+            for ((i, edge) in edges.withIndex())
+                add(Vertex(edge, i))
+        }
     }
-
 }
