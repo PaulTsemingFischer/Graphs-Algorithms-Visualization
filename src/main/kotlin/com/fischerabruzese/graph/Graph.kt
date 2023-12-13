0a5d1fdea3b6632b95f5326db96702851c73d885
paulfischer <24pfischer@xbhs.net>
2023-12-13 12:22:14 -0500

    Initialization and testing

--- src/main/kotlin/com/fischerabruzese/graph/Graph.kt
+++ src/main/kotlin/com/fischerabruzese/graph/Graph.kt
@@ -2,17 +2,17 @@ package com.fischerabruzese.graph
 
 class Graph<E:Any>(vararg outboundConnections : Pair<E,Array<Pair<E,Int>>>?) {
 
-    constructor(vararg outboundConnectionsUnweighted : Pair<E,Array<E>>) : this(*(
-                Array<Pair<E,Array<Pair<E,Int>>>?>(outboundConnectionsUnweighted.size) {
-                    i -> Pair(
-                    outboundConnectionsUnweighted[i].first,
-                    Array<Pair<E,Int>>(
-                        outboundConnectionsUnweighted[i].second.size) {
-                            j -> Pair(outboundConnectionsUnweighted[i].second[j],1)
-                        }
-                    )
-                }
-            ))
+//    constructor(vararg outboundConnectionsUnweighted : Pair<E,Array<E>>) : this(*(
+//                Array<Pair<E,Array<Pair<E,Int>>>?>(outboundConnectionsUnweighted.size) {
+//                    i -> Pair(
+//                    outboundConnectionsUnweighted[i].first,
+//                    Array<Pair<E,Int>>(
+//                        outboundConnectionsUnweighted[i].second.size) {
+//                            j -> Pair(outboundConnectionsUnweighted[i].second[j],1)
+//                        }
+//                    )
+//                }
+//            ))
 
     constructor(vararg vertices: E) : this(*(
                 Array<Pair<E,Array<Pair<E,Int>>>?>(vertices.size) { i -> Pair(
@@ -30,20 +30,43 @@ class Graph<E:Any>(vararg outboundConnections : Pair<E,Array<Pair<E,Int>>>?) {
 
     init {
         val addedVertices = HashMap<E, Int>()
-        for(edge in edges){
-            if(addedVertices.put(edge.from, vertices.size) == null){
-                vertices.add(Vertex(edge.from, vertices.size))
+        for(connections in outboundConnections){
+            if(connections == null) continue
+            if(addedVertices.put(connections.first, vertices.size) == null){
+                vertices.add(Vertex(connections.first, vertices.size))
             }
-            if(edge.to != null && addedVertices.put(edge.to, vertices.size) == null){
-                vertices.add(Vertex(edge.to, vertices.size))
+            for(outboundEdge in connections.second){
+                if(addedVertices.put(outboundEdge.first, vertices.size) == null){
+                    vertices.add(Vertex(outboundEdge.first, vertices.size))
+                }
             }
-        }
-        edgeMatrix = Array(vertices.size) {IntArray(vertices.size)}
-        for(edge in edges){
-            if(edge.to != null){
-                edgeMatrix[addedVertices[edge.from]!!][addedVertices[edge.to]!!] = edge.weight
-            }
-        }
 
+        }
+        edgeMatrix = Array(vertices.size) {IntArray(vertices.size) {-1} }
+
+        for(connections in outboundConnections) {
+            if(connections == null) continue
+            for(outboundEdge in connections.second){
+                edgeMatrix[addedVertices[connections.first]!!][addedVertices[outboundEdge.first]!!] = outboundEdge.second
+            }
+        }
     }
+
+    override fun toString(): String {
+        val string = StringBuilder()
+        for(destinations in edgeMatrix){
+            string.append("[")
+            for (weight in destinations){
+                string.append("$weight][")
+            }
+            string.deleteRange(string.length - 2, string.length)
+            string.append("]\n")
+        }
+        return string.toString()
+    }
+    fun getVertices() : ArrayList<Vertex<E>> {
+        return vertices
+    }
+
+
 }
