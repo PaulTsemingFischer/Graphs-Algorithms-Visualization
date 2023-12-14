8b9d5052ab3abf77477ae630f6189687a042a667
paulfischer <24pfischer@xbhs.net>
2023-12-14 08:20:45 -0500

    -Changed printing of -1 to " " -Changed vertices getter to printing for information hiding -Randomize method

--- src/main/kotlin/com/fischerabruzese/graph/Testing.kt
+++ src/main/kotlin/com/fischerabruzese/graph/Testing.kt
@@ -2,6 +2,7 @@ package com.fischerabruzese.graph
 
 fun main() {
     val graph = Graph<Int>(5 to ((0..4).map { it to 3} ))
+    graph.randomize(0.5, 10)
     println(graph)
-    println("Verts: ${graph.getVertices()}")
+
 }
