ec4192d69972fca1c84185a8149eccb3e9d1b84c
Paul F <paultsemingfischer@gmail.com>
2023-12-20 23:13:56 -0500

    -Allowing GraphGui to take in a graph(except not really since it doesn't work)

--- src/main/kotlin/com/fischerabruzese/graphsFX/GraphApp.kt
+++ src/main/kotlin/com/fischerabruzese/graphsFX/GraphApp.kt
@@ -4,6 +4,7 @@ import javafx.application.Application
 import javafx.fxml.FXMLLoader
 import javafx.scene.Scene
 import javafx.stage.Stage
+import java.lang.Exception
 
 class GraphApp : Application() {
     override fun start(stage: Stage) {
