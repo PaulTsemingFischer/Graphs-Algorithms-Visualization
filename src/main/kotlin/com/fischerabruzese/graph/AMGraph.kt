e7fe9113bf879e3838edb802675780462517d69b
Paul F <paultsemingfischer@gmail.com>
2023-12-24 00:34:04 -0500

    -Dijkstras

--- src/main/kotlin/com/fischerabruzese/graph/AMGraph.kt
+++ src/main/kotlin/com/fischerabruzese/graph/AMGraph.kt
@@ -1,5 +1,8 @@
 package com.fischerabruzese.graph
 
+import java.util.LinkedList
+import java.util.PriorityQueue
+
 /* Construct a graph with outboundConnections containing:
         outboundConnection(Source, ArrayOf(Destination, Weight))
  */
@@ -29,11 +32,11 @@ class AMGraph<E:Any>(vararg outboundConnections : Pair<E,Iterable<Pair<E,Int>>>?
     init {
         for(connections in outboundConnections){
             if(connections == null) continue
-            if(indexLookup.put(connections.first, vertices.size) == null){
+            if(indexLookup.putIfAbsent(connections.first, vertices.size) == null){
                 vertices.add(Vertex(connections.first))
             }
             for(outboundEdge in connections.second){
-                if(indexLookup.put(outboundEdge.first, vertices.size) == null){
+                if(indexLookup.putIfAbsent(outboundEdge.first, vertices.size) == null){
                     vertices.add(Vertex(outboundEdge.first))
                 }
             }
@@ -73,18 +76,6 @@ class AMGraph<E:Any>(vararg outboundConnections : Pair<E,Iterable<Pair<E,Int>>>?
         return get(from, to).also { edgeMatrix[from][to] = value }
     }
 
-    fun randomize(chanceFilled : Double, maxWeight: Int){
-        for(i in edgeMatrix.indices) {
-            for (j in edgeMatrix.indices) {
-                if (chanceFilled > Math.random()) {
-                    set(i, j, (1..maxWeight).random())
-                } else {
-                    set(i, j, -1)
-                }
-            }
-        }
-    }
-
     override fun iterator(): Iterator<E> {
         TODO("Not yet implemented")
     }
@@ -106,5 +97,135 @@ class AMGraph<E:Any>(vararg outboundConnections : Pair<E,Iterable<Pair<E,Int>>>?
         return string.toString()
     }
 
-    fun printVertices() = println("Vertices: $vertices")
+    fun randomize(chanceFilled : Double, maxWeight: Int){
+        for(i in edgeMatrix.indices) {
+            for (j in edgeMatrix.indices) {
+                if (chanceFilled > Math.random()) {
+                    set(i, j, (1..maxWeight).random())
+                } else {
+                    set(i, j, -1)
+                }
+            }
+        }
+    }
+
+    //Pre-condition: "from" is a valid vertex
+    fun getAllDijkstra(from : E) : Array<Pair<List<E>, Int>> {
+        val fromIndex = indexLookup[from]!!
+        return dikstra(fromIndex, null).let{
+            it.mapIndexed{ index, pair ->
+                getPath(fromIndex, index, it).map {vertex ->  vertices[vertex].item } to pair.second
+            }
+        }.toTypedArray()
+    }
+
+    fun getDijkstra(from: E, to: E) : Pair<List<E>, Int>{
+        val fromIndex = indexLookup[from]!!
+        val toIndex = indexLookup[to]!!
+        return dikstra(fromIndex, toIndex).let{
+            getPath(fromIndex, toIndex, it).map{vertex -> vertices[vertex].item} to it[toIndex].second
+        }
+    }
+
+    fun getDijkstraWeight(from: E, to: E) : Int{
+        val fromIndex = indexLookup[from]!!
+        val toIndex = indexLookup[to]!!
+        return dikstra(fromIndex, toIndex)[toIndex].second
+    }
+
+    fun getDijkstraPath(from: E, to: E) : List<E>{
+        return getDijkstra(from, to).first
+    }
+
+    fun getAllDijkstras(from : E) : Array<Pair<List<E>, Int>> {
+        val fromIndex = indexLookup[from]!!
+        return abruzeseDijkstras(fromIndex).let{
+            it.mapIndexed{ index, pair ->
+                abruzeseGetPath(index, it).map {vertex ->  vertices[vertex].item } to 0
+            }
+        }.toTypedArray()
+    }
+
+    private fun abruzeseDijkstras(from : Int): Array<LinkedList<Int>> {
+        data class DikstraVertex(val location : Int, var previous : DikstraVertex?, var distFromSrc : Int, var visited : Boolean)
+        var start : DikstraVertex? = null
+        val dikstrasVertices = List(vertices.size) { i -> DikstraVertex(i, null, Int.MAX_VALUE, false).also { if(i == from) start = it; it.distFromSrc = 0} }
+
+        val que = LinkedList<DikstraVertex>()
+        que.add(start!!)
+        while(que.size != 0) {
+            val cur = que.peek()
+            for ((next, distToNext) in edgeMatrix[cur.location].withIndex()) {
+                if (next != -1 && !dikstrasVertices[next].visited && dikstrasVertices[next].distFromSrc < cur.distFromSrc + distToNext) {
+                    dikstrasVertices[next].previous = cur
+                    dikstrasVertices[next].distFromSrc = cur.distFromSrc + distToNext
+                    que.add(dikstrasVertices[next])
+                }
+            }
+            que.poll().visited = true
+        }
+        //the index in the array is the path it is to
+        return Array(vertices.size) { i ->
+            var path = LinkedList<Int>()
+            var j = i
+            while(dikstrasVertices[j] != start){
+                if(dikstrasVertices[j].previous == null) {
+                    path = emptyList<Int>() as LinkedList<Int>
+                    break
+                }
+                else j = dikstrasVertices[j].previous!!.location
+                path.addFirst(dikstrasVertices[j].location)
+            }
+            path
+        }
+    }
+    private fun abruzeseGetPath(to: Int, table : Array<LinkedList<Int>>): List<Int> {
+        return table[to]
+    }
+
+
+    //Pre-condition: If "to" is null, finds every path from "from", else only the path from "from" to "to" is accurate
+    //Post-condition: A Int.MAX_VALUE in distance indicates unreachable, a -1 in Prev indicates no path
+    //Post-condition: Returns an array of (previous vertex index, distance)
+    private fun dikstra(from : Int, to : Int?) : Array<Pair<Int, Int>> {
+        val distance = IntArray(vertices.size) { Int.MAX_VALUE }
+        val prev = IntArray(vertices.size) { -1 }
+        val visited = BooleanArray(vertices.size) { false }
+
+        distance[from] = 0
+        while(to == null || !visited[to]){
+            //Determine the next vertex to visit
+            var currVert = visited.indexOfFirst{!it} //Finds first unvisited
+            if(currVert == -1 || distance[currVert] == Int.MAX_VALUE) break //All visited//TODO: might cause issues
+            for(i in currVert + 1 until visited.size){//TODO: +1 might cause issues
+                if(!visited[i] && distance[i] < distance[currVert]){
+                    currVert = i
+                }
+            }
+
+            //Update distances and previous
+            val currDist = distance[currVert]
+            for(i in visited.indices){
+                val edge = edgeMatrix[currVert][i]
+                if(!visited[i] && edge != -1 && currDist + edge < distance[i]){
+                    distance[i] = (currDist + edgeMatrix[currVert][i])
+                    prev[i] = currVert
+                }
+            }
+
+            //Update visited
+            visited[currVert] = true
+        }
+        return prev.zip(distance).toTypedArray() //funky function
+    }
+
+    private fun getPath(from: Int, to: Int, dikstraTable: Array<Pair<Int, Int>>): List<Int> {
+        val path = LinkedList<Int>()
+        path.add(to)
+        var curr = to
+        while(path.firstOrNull() != from){
+            path.addFirst(dikstraTable[curr].first.also{curr = it})
+        }
+        return path
+    }
 }
--- src/main/kotlin/com/fischerabruzese/graphsFX/GraphApp.kt
+++ src/main/kotlin/com/fischerabruzese/graphsFX/GraphApp.kt
@@ -5,20 +5,31 @@ import javafx.application.Application
 import javafx.fxml.FXMLLoader
 import javafx.scene.Scene
 import javafx.stage.Stage
-import java.lang.Exception
 
 class GraphApp : Application() {
     override fun start(stage: Stage) {
         val fxmlLoader = FXMLLoader(GraphApp::class.java.getResource("graph.fxml"))
         val scene = Scene(fxmlLoader.load(), 600.0, 400.0)
-        val controller : Controller<Int> = fxmlLoader.getController()!!
+        val controller : Controller<Char> = fxmlLoader.getController()!!
 
         stage.title = "Graph"
         stage.scene = scene
 
-        val graph = AMGraph(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
-        graph.randomize(0.3, 9)
+        val graph = AMGraph('a' to listOf('c' to 2, 'd' to 7), 'b' to listOf('c' to 7, 'd' to 1), 'c' to listOf('a' to 2, 'b' to 4, 'd' to 3), 'd' to listOf('a' to 7, 'b' to 1, 'c' to 3))
         controller.graphInit(graph)
+
+        println("------------Dikstra------------")
+        println("From a:" + graph.getAllDijkstra('a').joinToString("\n"))
+        println("From b:" + graph.getAllDijkstra('b').joinToString("\n"))
+        println("From c:" + graph.getAllDijkstra('c').joinToString("\n"))
+        println("From d:" + graph.getAllDijkstra('d').joinToString("\n"))
+
+
+        println("------------Dikstras------------")
+        println("From a:" + graph.getAllDijkstras('a').joinToString("\n"))
+        println("From b:" + graph.getAllDijkstras('b').joinToString("\n"))
+        println("From c:" + graph.getAllDijkstras('c').joinToString("\n"))
+        println("From d:" + graph.getAllDijkstras('d').joinToString("\n"))
 
         controller.draw()
         stage.show()
