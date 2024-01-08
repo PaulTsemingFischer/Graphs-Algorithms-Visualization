b2d7986954119bc03ed864be5367c0e1c0cddc4b
paulfischer <24pfischer@xbhs.net>
2024-01-08 09:51:26 -0500

    -Made edges composed of 2 triangles(it doesn't work)

--- src/main/kotlin/com/fischerabruzese/graphsFX/Controller.kt
+++ src/main/kotlin/com/fischerabruzese/graphsFX/Controller.kt
@@ -14,7 +14,7 @@ import javafx.scene.layout.Pane
 import javafx.scene.layout.StackPane
 import javafx.scene.paint.Color
 import javafx.scene.shape.Circle
-import javafx.scene.shape.Line
+import javafx.scene.shape.Polygon
 
 class Controller<E: Any> {
     @FXML
@@ -35,8 +35,8 @@ class Controller<E: Any> {
 
     private lateinit var graph : Graph<E>
     private val stringToEMap = HashMap<String, E>()
-    private val edges = ArrayList<Edge>()
-    private val vertices = ArrayList<Vertex>()
+    private var edges = ArrayList<Edge>()
+    private var vertices = ArrayList<Vertex>()
     private val hitboxes = ArrayList<Circle>()
 
     @FXML
@@ -53,21 +53,23 @@ class Controller<E: Any> {
     }
 
     fun draw() {
-        val vertices = graph.getVertices().toList()
-        val verticesElements = Array(vertices.size){ index -> Vertex(vertices[index].toString(), Math.random(), Math.random())}
+        val graphVertices = graph.getVertices().toList()
+        val verticesElements = Array(graphVertices.size){ index -> Vertex(graphVertices[index].toString(), Math.random(), Math.random())}
         val edgeElements = ArrayList<Edge>()
 
         for(i in verticesElements.indices){
             for(j in i until verticesElements.size){
-                val weight = graph[vertices[i], vertices[j]] ?: continue
-                edgeElements.add(Edge(verticesElements[i], verticesElements[j], weight))
+                val outBoundWeight = graph[graphVertices[i], graphVertices[j]] ?: continue
+                val inBoundWeight = graph[graphVertices[j], graphVertices[i]] ?: continue
+                edgeElements.add(Edge(verticesElements[i], verticesElements[j], outBoundWeight, inBoundWeight))
             }
         }
-        edges.addAll(edgeElements)
-        this.vertices.addAll(verticesElements)
+        edges = edgeElements
+        vertices = ArrayList(verticesElements.asList())
         pane.children.addAll(edgeElements)
         pane.children.addAll(verticesElements)
         pane.children.addAll(hitboxes)
+        for(edge in edgeElements) edge.initialize()
     }
 
 
@@ -85,8 +87,9 @@ class Controller<E: Any> {
         graph[from, to] = weight.toInt()
         for(edge in edges){
             if(edge.checkMatch(from, to)){
-                println(edge.label)
-                edge.label.text = weight
+                edge.setLabelWeight(weight, true)
+            } else if(edge.checkMatch(to, from)){
+                edge.setLabelWeight(weight, false)
             }
         }
     }
@@ -113,8 +116,8 @@ class Controller<E: Any> {
         private var xpos : DoubleProperty = SimpleDoubleProperty(x)
         private var ypos : DoubleProperty = SimpleDoubleProperty(y)
 
-        private var xBinding : DoubleBinding = paneWidth.multiply(xpos).multiply(usablePercentPaneWidth)
-        private var yBinding : DoubleBinding = paneHeight.multiply(ypos).multiply(usablePercentPaneHeight)
+        var vtranslateXProperty : DoubleBinding = paneWidth.multiply(xpos).multiply(usablePercentPaneWidth)
+        var vtranslateYProperty : DoubleBinding = paneHeight.multiply(ypos).multiply(usablePercentPaneHeight)
 
         //Dragging
         private var xDelta : Double = 0.0
@@ -122,18 +125,18 @@ class Controller<E: Any> {
 
         init{
             //Circle
-            circle.translateXProperty().bind(xBinding)
-            circle.translateYProperty().bind(yBinding)
+            circle.translateXProperty().bind(vtranslateXProperty)
+            circle.translateYProperty().bind(vtranslateYProperty)
 
             //Label
-            label.translateXProperty().bind(circle.translateXProperty())
-            label.translateYProperty().bind(circle.translateYProperty())
+            label.translateXProperty().bind(vtranslateXProperty)
+            label.translateYProperty().bind(vtranslateYProperty)
 
             label.textFill = Color.WHITE
 
             //Hitbox
-            hitbox.translateXProperty().bind(circle.translateXProperty())
-            hitbox.translateYProperty().bind(circle.translateYProperty())
+            hitbox.translateXProperty().bind(vtranslateXProperty)
+            hitbox.translateYProperty().bind(vtranslateYProperty)
 
             hitbox.centerX = CIRCLE_RADIUS
             hitbox.centerY = CIRCLE_RADIUS
@@ -162,30 +165,30 @@ class Controller<E: Any> {
         }
 
         private fun greyNonAttached(vertex: Vertex){
-            for(vert in vertices){
-                vert.circle.fill = Color(0.0, 0.0, 1.0, 0.3)
-            }
-            for(edge in edges){
-                if(edge.from != vertex && edge.to != vertex){
-                    edge.label.textFill = Color.GREY
-                    edge.line.stroke = Color.rgb(192, 192, 192, 0.8)
-                }
-                else{
-                    edge.from.let{if(it != this) it.circle.fill = Color.BLUE}
-                    edge.to.let{if(it != this) it.circle.fill = Color.BLUE}
-                    edge.line.stroke = Color.RED
-                }
-            }
+//            for(vert in vertices){
+//                vert.circle.fill = Color(0.0, 0.0, 1.0, 0.3)
+//            }
+//            for(edge in edges){
+//                if(edge.from != vertex && edge.to != vertex){
+//                    edge.label.textFill = Color.GREY
+//                    edge.line.stroke = Color.rgb(192, 192, 192, 0.8)
+//                }
+//                else{
+//                    edge.from.let{if(it != this) it.circle.fill = Color.BLUE}
+//                    edge.to.let{if(it != this) it.circle.fill = Color.BLUE}
+//                    edge.line.stroke = Color.RED
+//                }
+//            }
         }
 
         private fun ungreyEverything(){
-            for(edge in edges){
-                edge.label.textFill = Color.BLACK
-                edge.line.stroke = Color.BLACK
-            }
-            for (vert in vertices){
-                vert.circle.fill = Color.BLUE
-            }
+//            for(edge in edges){
+//                edge.label.textFill = Color.BLACK
+//                edge.line.stroke = Color.BLACK
+//            }
+//            for (vert in vertices){
+//                vert.circle.fill = Color.BLUE
+//            }
         }
         fun getCenterX() : DoubleBinding {
             return circle.translateXProperty().add(circle.radiusProperty())
@@ -198,25 +201,105 @@ class Controller<E: Any> {
             ypos.set(y)
         }
     }
-    inner class Edge(val from : Vertex, val to : Vertex, weight : Int) : Pane() {
-        val line = Line()
-        var label = Label(weight.toString())
+    inner class Edge(private val from : Vertex, private val to : Vertex, outBoundWeight : Int, inBoundWeight: Int) : Pane() {
+        private val outBound = Polygon()
+        private val inBound = Polygon()
+        private var outBoundLabel = Label(outBoundWeight.toString())
+        private var inBoundLabel = Label(outBoundWeight.toString())
 
         init{
-            //Binding endpoints
-            line.startXProperty().bind(from.getCenterX())
-            line.startYProperty().bind(from.getCenterY())
-            line.endXProperty().bind(to.getCenterX())
-            line.endYProperty().bind(to.getCenterY())
+            //Initializing triangles
+            outBound.fill = Color.BLACK
+            inBound.fill = Color.BLACK
 
-            //Binding label position to the midpoint of the line
-            label.translateXProperty().bind((line.startXProperty().add(line.endXProperty())).divide(2))
-            label.translateYProperty().bind((line.startYProperty().add(line.endYProperty())).divide(2))
-            label.textFill = Color.BLACK
+            //Initializing labels
+            outBoundLabel.textFill = Color.BLACK
+            inBoundLabel.textFill = Color.BLACK
 
-            children.addAll(line, label)
+            //Binding triangle vertices and labels
+                //Outbound
+            from.vtranslateXProperty.addListener {_ ->updateTrianglePoints(true); updateLabelPosition(outBoundLabel) }
+            from.vtranslateXProperty.addListener {_ -> updateTrianglePoints(true); updateLabelPosition(outBoundLabel) }
+
+            to.vtranslateXProperty.addListener {_ -> updateTrianglePoints(true); updateLabelPosition(outBoundLabel) }
+            to.vtranslateXProperty.addListener {_ -> updateTrianglePoints(true); updateLabelPosition(outBoundLabel) }
+
+                //Inbound
+            from.vtranslateXProperty.addListener {_ -> updateTrianglePoints(false); updateLabelPosition(inBoundLabel) }
+            from.vtranslateXProperty.addListener {_ -> updateTrianglePoints(false); updateLabelPosition(inBoundLabel) }
+
+            to.vtranslateXProperty.addListener {_ -> updateTrianglePoints(false); updateLabelPosition(inBoundLabel) }
+            to.vtranslateXProperty.addListener {_ -> updateTrianglePoints(false); updateLabelPosition(inBoundLabel) }
+
+            children.addAll(outBound, inBound, outBoundLabel, inBoundLabel)
+        }
+
+        fun initialize() {
+            updateTrianglePoints(true)
+            updateTrianglePoints(false)
+            updateLabelPosition(outBoundLabel)
+            updateLabelPosition(inBoundLabel)
         }
 
         fun checkMatch(from: E, to: E): Boolean = this.from.name.also{println(it)} == from.toString().also{println(it)} && this.to.name == to.toString()
+
+        fun setLabelColor(color: Color){
+            outBoundLabel.textFill = color
+            inBoundLabel.textFill = color
+        }
+
+        fun setLabelWeight(weight: String, isOutbounds: Boolean){
+            if(isOutbounds) outBoundLabel.text = weight
+            else inBoundLabel.text = weight
+        }
+
+        fun setTriangleColor(color: Color, isOutbounds: Boolean){
+            if(isOutbounds) outBound.fill = color
+            else inBound.fill = color
+        }
+
+        //Label helper methods
+        private fun updateLabelPosition(label: Label){
+            val midpoint = calculateMidpoint(outBound)
+            label.translateX = midpoint.first
+            label.translateY = midpoint.second
+        }
+
+        private fun calculateMidpoint(polygon: Polygon): Pair<Double, Double> {
+            val xSum = polygon.points.subList(0, polygon.points.size / 2).sum()
+            val ySum = polygon.points.subList(polygon.points.size / 2, polygon.points.size).sum()
+
+            val xMidpoint = xSum / (polygon.points.size / 2)
+            val yMidpoint = ySum / (polygon.points.size / 2)
+
+            return Pair(xMidpoint, yMidpoint)
+        }
+
+        //Triangle helper methods
+        private fun updateTrianglePoints(isOutbounds: Boolean){
+            println("From: ${from.vtranslateXProperty.get()} ${from.vtranslateYProperty.get()}")
+            println("To: ${to.vtranslateXProperty.get()} ${to.vtranslateYProperty.get()}")
+            if(isOutbounds){
+                updateTrianglePoints(outBound,
+                    from.vtranslateXProperty.get() to from.vtranslateYProperty.get(),
+                    from.vtranslateXProperty.get() + CIRCLE_RADIUS/2 to from.vtranslateYProperty.get() + CIRCLE_RADIUS/2,
+                    to.vtranslateXProperty.get() + CIRCLE_RADIUS/2 to to.vtranslateYProperty.get() + CIRCLE_RADIUS/2)
+            } else {
+                updateTrianglePoints(inBound,
+                    to.vtranslateXProperty.get() to to.vtranslateYProperty.get(),
+                    to.vtranslateXProperty.get() - CIRCLE_RADIUS/2 to to.vtranslateYProperty.get() - CIRCLE_RADIUS/2,
+                    from.vtranslateXProperty.get() - CIRCLE_RADIUS/2 to from.vtranslateYProperty.get() - CIRCLE_RADIUS/2)
+            }
+        }
+
+        private fun updateTrianglePoints(triangle: Polygon, point1: Pair<Double, Double>, point2: Pair<Double, Double>, point3: Pair<Double, Double>) {
+            triangle.points.clear()
+            triangle.points.addAll(
+                point1.first, point1.second,
+                point2.first, point2.second,
+                point3.first, point3.second
+            )
+            println(triangle.points)
+        }
     }
 }
